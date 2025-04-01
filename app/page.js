47bb9f6690f29dbfa5fa213c8e385737e7d16c48
jerry <oluwamayowajeremiah62@gmailcom>
2025-04-01 21:26:47 +0100

    fresh commit

---
+++ app/page.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import CurrentWeather from './components/CurrentWeather'
+
+const page = () => {
+  return (
+    <div className='bg-[#192735] min-h-screen'>
+      <CurrentWeather/>
+       </div>
+  )
+}
+
+export default page
